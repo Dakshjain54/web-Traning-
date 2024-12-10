3cb17bd44e2a50a3e84eb75daf59c26c9bd0b5b6
Dakshjain54 <daksh225j@gmail.com>
2024-12-10 10:34:02 +0530

    Add files via upload

---
+++ day 5/note.js
@@ -0,0 +1,66 @@
+console.log("......Welcome in js......")
+
+// const a = 5;
+// let b = 6
+// var r = a + b
+num1 = 5
+num2 = 6
+num3 = 7
+num4 = 8
+num5 = 99
+console.log(num1+num2+num3+num4+num5)
+console.log(num1+num2+num3+num4+num5/5)
+// // if (num1>num2) {
+// //     console.log(num1, "is greater then", num2)
+
+// // }
+// // else(
+// //     console.log(num2,"is greater then",num1)
+// // )
+// let num1 = prompt("Enter num1")
+// let num2 = prompt("Enter num2")
+// let num3 = prompt("Enter num3")
+// let num4 = prompt("Enter num4")
+// if (num1>=num2 && num1>=num3 && num1>=num4) {
+    
+//     console.log(num1 ,"is greater then", num2 ,num3 ,num4)
+//     document.body.style.backgroundColor = "red"
+// }
+// else if (num1=num2=num3=num4) {
+    
+//     console.log("all are equal",num1 , num2 ,num3 ,num4)
+//     document.body.style.backgroundColor = "orange"
+// }
+// else if(num2>=num1 && num2>=num3 && num2>=num4){
+//     console.log(num2, "is greater then" ,num1 ,num3 ,num4)
+//     document.body.style.backgroundColor = "blue"
+// }
+// else if(num3>=num1 && num3>=num2 && num3>=num4){
+//     console.log(num3, "is greater then" ,num1 ,num2 ,num4)
+//     document.body.style.backgroundColor = "green"
+// }
+// else{
+//     console.log(num4, "is greater then" ,num2 ,num1 ,num3)
+//     document.body.style.backgroundColor = "yellow"
+// }
+// let value = prompt("Enter value")
+// for(let i = 1; i <= 10; i++){
+//    const tableLine = value + " * "  + i + " = " + value * i
+//    console.log(tableLine)
+// }
+
+// for(let i = 0; i<=100; i++){
+//     if(i % 3 === 0){
+//       console.log(i)
+//     }  
+// }
+// for(let i = 0; i<=100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//       console.log(i)
+//     }  
+// }
+// for(let i = 0; i<=100; i++){
+//     if(i % 3 === 0 && i % 5 === 0 && i % 9 === 0){
+//       console.log(i)
+//     }  
+// }
